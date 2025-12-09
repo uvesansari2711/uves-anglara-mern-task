acd8622c50e40ea6de35be41dd19a209194ee96d
uvesansari2711 <uves.developer@gmail.com>
2025-12-09 13:54:32 +0530

    .env changes

--- tests/jest.setup.ts
+++ tests/jest.setup.ts
@@ -1,8 +1,4 @@
-// tests/jest.setup.ts
-
 // Set test-specific environment variables
 process.env.JWT_SECRET = process.env.JWT_SECRET || "testsecret";
 process.env.JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || "7d";
-
-// Optional but nice to have
 process.env.NODE_ENV = 'test';
